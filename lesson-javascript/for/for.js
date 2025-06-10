1cc357d387f39cc1f64d36eb1c69251db8a521a5
Jose Andrade <ignacio.andrademec@gmail.com>
2025-06-09 20:08:31 -0500

    actualización

---
+++ lesson-javascript/for/for.js
@@ -0,0 +1,35 @@
+
+
+
+
+// const estudiantes = [
+//     {nombre: "pepito", calificacion: 92},
+//     {nombre: "juanito", calificacion: 78},
+//     {nombre: "maria", calificacion: 65},
+//     {nombre: "sofia", calificacion: 85},
+//     {nombre: "juanita", calificacion: 50},
+// ];
+
+// for (let b=0; b<estudiantes.length; b++){
+//     const estudianteActual = estudiantes[b];
+
+//     let calificacionLetra = "";
+
+//     if(estudianteActual.calificacion >=90){
+//         calificacionLetra = "A";
+//     } else if (estudianteActual.calificacion >=80) {
+//         calificacionLetra = "B";
+//     } else if (estudianteActual.calificacion >=70) {
+//         calificacionLetra = "C";
+//     } else if (estudianteActual.calificacion >=60) {
+//         calificacionLetra = "D";
+//     }
+// }
+
+
+
+/* Como imprimir los números del 20 al 30*/
+
+for (let i = 0; i < 30; i++) {
+    console.log(i)
+}
