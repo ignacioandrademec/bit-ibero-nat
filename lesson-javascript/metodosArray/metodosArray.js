22e4e5c756cdc4d4725b97d22faf8ff7ad16c5a1
Jose Andrade <ignacio.andrademec@gmail.com>
2025-06-09 22:10:44 -0500

    Actualización de clase #1

---
+++ lesson-javascript/metodosArray/metodosArray.js
@@ -0,0 +1,57 @@
+"use strict"
+
+/* -------------------------------------------- METODO PUSH - AGREGAR---------------*/
+
+// push()  /* Agrega uno o mas elementos al final el arreglo */
+
+let misJuguetes = ["avion", "tren", "pelota"];
+
+console.log(misJuguetes);
+misJuguetes.push("carro", "nave espacial");
+console.log("Ahora estos son todos mis juguetes son:", misJuguetes);
+
+/* -------------------------------------------- METODO POP - ELIMINAR --------------*/
+
+// push()  /* Elimina el ultimo elemento del Array */
+
+let quitarUltimoJuguete = misJuguetes.pop();
+console.log("Ahora estos son todos mis juguetes son:", misJuguetes);
+
+/* -------------------------------------------- METODO UNSHIFT ---------------------*/
+
+// unshift()  /* Agrega uno o mas elementos al inicio del arreglo */
+
+misJuguetes.unshift("nave espacial", "bate");
+console.log("Ahora estos son todos mis juguetes son:", misJuguetes);
+
+/* -------------------------------------------- METODO SHIFT  ---------------------*/
+
+// shift()  /* Elimina el primer elemento de la lista */
+
+misJuguetes.shift();
+console.log("Ahora estos son todos mis juguetes son:", misJuguetes);
+
+
+/* ! -------------------------------------------- METODO LIFO  ---------------------*/
+
+// LIFO last in, first out /* El ultimo en entrar es el primero en salir */
+
+let miPilaDeTareas = [];
+console.log("Este es mi pila de tareas: ")
+
+
+/* ! -------------------------------------------- TAREA PENDIENTE PARA PRACTICAR------*/
+
+/* Usar un ejemplo con los diferentes metodos Array */
+
+
+
+
+
+
+
+
+// TODO 
+//! COMENTARIO DE ADVERTENCIA
+//* COMENTARIO BRILLANTE
+//? COMENTARIO PREGUNTA
