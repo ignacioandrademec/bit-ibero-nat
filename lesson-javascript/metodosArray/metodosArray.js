9a4b15675845dac192eeee97f50d4f7e316caf1e
Jose Andrade <ignacio.andrademec@gmail.com>
2025-06-12 15:30:44 -0500

    Adelanto ejercicio en clase

--- lesson-javascript/metodosArray/metodosArray.js
+++ lesson-javascript/metodosArray/metodosArray.js
@@ -301,6 +301,311 @@ let totalPuntos = num2.reduce(function(acomulador, puntuacionActual){
 console.log("El total de puntos es: ", totalPuntos);
 
 
+// ? ----------------------------------------- FIND -----------------------------------------
+
+/* Devuelve el valor del primer elemento que encuentre, que cumpla con el parámetro proporcionado, si no lo encuentra entonces nos entrega un undefined*/
+
+let rangosEmpleado2 = [
+    {
+        nombre: "Andrea", rango: "admin"
+    },{
+        nombre: "Cristian", rango: "user"
+    },{
+        nombre: "Diana", rango: "admin"
+    },{
+        nombre: "Horacio", rango: "user"
+    },{
+        nombre: "Jaderson", rango: "admin"
+    }
+];
+
+let primerRangoEncontrado = rangosEmpleado2.find(function(rol){
+    return rol.rango === "user";
+})
+
+console.log("Primer rango encontrado: ", primerRangoEncontrado);
+
+
+// ? -------------------------------- FINDINDEX - FILTER - FIND ---------------------------
+
+/* Devuelve el indice del primer elemento, que encuentre que cumpla con la funcion de prueba*/
+
+
+let misPeliculas = [
+    {
+        nombre: "Harry Potter", genero: "fantasia"
+    },
+    {
+        nombre: "Fast and furious", genero: "accion"
+    },
+    {
+        nombre: "Forest Gump", genero: "drama"
+    },
+    {
+        nombre: "Misión Imposible", genero: "accion"
+    },
+    {
+        nombre: "Lilo y Stitch", genero: "fantasia"
+    },
+    {
+        nombre: "El señor de los anillos", genero: "fantasia"
+    }
+]
+
+let indicePelicula = misPeliculas.findIndex(function(gen){
+    return gen.genero === "drama"
+});
+
+let primerGenero = misPeliculas.find(function(gen){
+    return gen.genero === "accion"
+});
+
+let segundoGenero = misPeliculas.filter(function(gen){
+    return gen.genero === "accion"
+});
+
+console.log("La primera posición del genero de acción que se encontro fue: ", indicePelicula);
+console.log("La primera coincidencia con el genero de acción que se encontro fue: ", primerGenero);
+console.log("La primera posición del genero de acción que se encontro fue: ", segundoGenero);
+
+// ? ----------------------------------------- SOME-----------------------------------------
+
+/* Comprobar si al menos un elemento del array cumple con la condición implementada por la funcion indicada*/
+
+let misPeliculas2 = [
+    {
+        nombre: "Harry Potter", genero: "fantasia"
+    },
+    {
+        nombre: "Fast and furious", genero: "accion"
+    },
+    {
+        nombre: "Forest Gump", genero: "drama"
+    },
+    {
+        nombre: "Misión Imposible", genero: "accion"
+    },
+    {
+        nombre: "Lilo y Stitch", genero: "fantasia"
+    },
+    {
+        nombre: "El señor de los anillos", genero: "fantasia"
+    }
+]
+
+let algunElemento = misPeliculas2.some(function(name){
+    return name.nombre === "El señor de los anillos"
+})
+
+console.log("Existe algun elemento que se llame 'El señor de los anillos'?: ", algunElemento);
+
+
+// ? ----------------------------------------- EVERY -----------------------------------------
+
+/* Comprobar si todos los elementos del array cumplen con la condición implementada por la funcion indicada*/
+
+
+let misPeliculas3 = [
+    {
+        nombre: "Harry Potter", genero: "fantasia"
+    },
+    {
+        nombre: "Fast and furious", genero: "accion"
+    },
+    {
+        nombre: "Forest Gump", genero: "drama"
+    },
+    {
+        nombre: "Misión Imposible", genero: "accion"
+    },
+    {
+        nombre: "Lilo y Stitch", genero: "fantasia"
+    },
+    {
+        nombre: "El señor de los anillos", genero: "fantasia"
+    }
+];
+
+let todasLasPeliculasSon = misPeliculas3.every(function(gen1){
+    return gen1.nombre === "El señor de los anillos"
+});
+
+console.log("Todas las peliculas son de acción?: ", todasLasPeliculasSon);
+
+
+// ? ----------------------------------------- CONCAT -----------------------------------------
+
+/* Unir dos o mas Array, este metodo no cambia los array existentes mas bien devuelve uno nuevo con la unión minimo se pueden usar dos*/
+
+let misJuguetes = ["Carro", "Tren", "Pelota"];
+
+let nuevosJuguetes = ["Robot", "Bici", "Piano"];
+
+let totalJuguetes = misJuguetes.concat(nuevosJuguetes);
+
+console.log("En total quede con estos juguetes: ", totalJuguetes);
+
+// ? ----------------------------------------- SLICE -----------------------------------------
+
+/* Devuelve una copia superficial de una porcion del Array dentro de un nuevo array, tomando un rango de la posición 1 a la 4*/
+
+let filaAmigos = ["Valentina", "Tobias", "Sebastian", "Samuel", "Nicolas"]
+console.log(filaAmigos);
+
+
+let amigosMedio = filaAmigos.slice(1,3);
+console.log("Los amigos del medio son: ", amigosMedio);
+
+
+// TODO -------------------------------------------- EJERCICIO EN CLASE - ARREGLOS -------
+
+// ! -------- GESTIÓN DE UNA COLECCIÓN DE LIBROS -----------------------------------------
+
+/* Estas desarrollando un sistema de gestión para una pequeña biblioteca. Para ello, necesitas manejar una colacción de libros, donde cada libro es un objeto con las siguientes propiedades: 
+
+- id: Número que identifica al libro.
+- titulo: El título del libro (string)
+- autor: El autor del libro (string)
+- añopublicacion: El año en que se publico el libro (número)
+- genero: El género del libro (string)
+- disponible: Un booleano que indica si el libro esta disponible para un préstamo (TRUE-FALSE)
+
+Tu tarea es crear uno o más arrays de objetos (libros) y luego realizar las siguientes operaciones utilizando los metodos de array apropiados:
+
+INICIALIZACIÓN: 
+
+- Crea un array inicial de libro con al menos 5 objetos.
+
+AGREGAR LIBROS:
+
+- Agrega un nuevo libro al final de la colección.
+- Agrega un nuevo libro al inicio de la colección.
+
+ELIMINAR LIBROS:
+
+- Elimina el último libro de la colección.
+- Elimina el primer libro de la colección.
+- Elimina un lbro especifico de la colección por su ID (debes encontrar su índice primero).
+
+ACTUALIZAR / MODIFICAR LIBROS:
+
+- Inserta un nuevo libro en una posición específica del array sin reemplazar un exisitente.
+- Reemplaza uno o mas libros en una posición especifica del array.
+
+CONSULTA Y FILTRADO:
+
+- Crea un nuevo Array con todos los libros publicados despues del año 2000.
+- Encuentra el primer libro cuyo género sea "ciencia ficción", y este disponible.
+- Encuentra el índice del libro con el ID 103 (o cualquier ID que exista en tu coleeción).
+
+*/
+
+// ? --------------------------------------- SOLUCIÓN ------------------------------------
+
+
+let coleccion =[
+    {
+        id:1, titulo:"Cien años de Soledad",autor:"Gabriel Garcia Marquez",añoPublicacion:1967,genero:"Fantasía",disponible:true
+    },{
+        id:2, titulo:"1984",autor:"George Orwell",añoPublicacion:1949,genero:"Distopía",disponible:false
+    },{
+        id:3, titulo:"La rebelión de las ratas",autor:"Fernando Soto",añoPublicacion:1962,genero:"Realismo",disponible:true
+    },{
+        id:4, titulo:"Don Quijote De la Mancha",autor:"Miguel De Cervantes",añoPublicacion:1605,genero:"Sátira",disponible:true
+    },{
+        id:5,titulo: "El Señor de los Anillos: La Comunidad del Anillo",autor: "J.R.R. Tolkien",añoPublicacion: 1954,genero: "Fantasía",disponible: false
+    }
+];
+
+// ! -------- ADICIÓN LIBRO AL FINAL ---------------------------------------------------
+
+coleccion.push(
+    {
+    id: 6,
+    titulo: "El Silmarillion",
+    autor: "J.R.R. Tolkien",
+    añoPublicacion: 1977,
+    genero: "Fantasía",
+    disponible: true
+    }
+);
+console.log("Colección, libro agregado al final: ", coleccion);
+
+// ! -------- ADICIÓN LIBRO AL INICIO ---------------------------------------------------
+
+coleccion.unshift(
+    {
+    id: 7,
+    titulo: "El principito",
+    autor: "Antoine de Saint-Exupery",
+    añoPublicacion: "1943",
+    genero: "Literatura infantil",
+    disponible: false,
+    }
+);
+console.log("Colección, libro agregado al inicio: ", coleccion);
+
+// ! -------- ELIMINA EL LIBRO DEL FINAL ---------------------------------------------------
+
+coleccion.pop(
+    {
+    id: 6,
+    titulo: "El Silmarillion",
+    autor: "J.R.R. Tolkien",
+    añoPublicacion: 1977,
+    genero: "Fantasía",
+    disponible: true
+    }
+);
+console.log("Colección, libro eliminado al final: ", coleccion);
+
+// ! -------- ELIMINA EL LIBRO DEL INICIO --------------------------------------------------
+
+coleccion.shift(
+    {
+    id: 7,
+    titulo: "El principito",
+    autor: "Antoine de Saint-Exupery",
+    añoPublicacion: "1943",
+    genero: "Literatura infantil",
+    disponible: false,
+    }
+);
+console.log("Colección, libro eliminado al inicio: ", coleccion);
+
+
+// ! -------- ELIMINA EL LIBRO EN ESPECÍFICO / BUSCAR ID --------------------------------
+
+
+let buscarId = coleccion.findIndex(function(find){
+    return find.id === 3
+})
+
+console.log("Busqueda del libro con id:3 el indice es =>", buscarId);
+
+
+// ! -------- ELIMINA EL LIBRO POR SU ID ------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
